5cbe40d8292c88d536487c932c793fca44c8f0fb
stoicandy99 <andy@andysmart.co.uk>
2025-05-10 13:39:20 +0100

    Added clock object to manage FPS.

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ import pygame
 from constants import *
 
 def main():
+    # Initialize pygame
     pygame.init()
     print ("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Set up some time shizzle
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # The main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
